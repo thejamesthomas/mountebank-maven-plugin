3e3b29de2ea6f8ff2b84ef80e2d674ec7cd45fc9
James Thomas <thejamesthomas@gmail.com>
2015-04-12 17:46:33 -0500

    Moving logic for parsing download page into a seperate class.

---
+++ src/test/java/org/mbtest/mountebank/DownloadPageScraperTest.java
@@ -0,0 +1,29 @@
+package org.mbtest.mountebank;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class DownloadPageScraperTest {
+    @Test
+    public void shouldGetUrlForOSXBinaryWhenOnOSX() throws IOException {
+        System.setProperty("os.name", OS.OSX.getValue());
+
+        Connection connection = mock(Connection.class);
+        Document document = Jsoup.parse(new File("src/test/resources/mbtest.html"), "UTF-8");
+        when(connection.get()).thenReturn(document);
+
+        DownloadPageScraper pageParser = new DownloadPageScraper(connection);
+        String binaryUrl = pageParser.getLatestBinaryUrl();
+
+        assertThat(binaryUrl).isEqualTo("https://s3.amazonaws.com/mountebank/v1.2/mountebank-v1.2.56-darwin-x64.tar.gz");
+    }
+}
